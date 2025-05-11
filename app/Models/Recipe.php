55da6311f3f3196f3987b182899573b8188f5a3f
Calebe Copello <calebecopello@gmail.com>
2025-05-10 21:53:35 -0300

    added relations to Recipe Model

---
+++ app/Models/Recipe.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Recipe extends Model
+{
+    use SoftDeletes, HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'ingredients',
+        'instructions',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
