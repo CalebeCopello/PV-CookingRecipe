c67d0b1fa98b6ced63bb642c8dcfd823264f4da7
Calebe Copello <calebecopello@gmail.com>
2025-05-10 21:54:16 -0300

    added protected routes

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,21 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RecipeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+//Auth Routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-
 Route::middleware('auth:sanctum')->delete('/logout', [AuthController::class, 'logout']);
+
+//Recipes Routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/recipes', [RecipeController::class, 'index']);
+    Route::post('/recipes', [RecipeController::class, 'store']);
+    Route::get('/recipes/{id}', [RecipeController::class, 'show']);
+    Route::put('/recipes/{id}', [RecipeController::class, 'update']);
+    Route::delete('/recipes/{id}', [RecipeController::class, 'destroy']);
+});
