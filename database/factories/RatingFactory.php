96a23cda4d3cedd35daa597e8d2ed0d99f158d71
Calebe Copello <calebecopello@gmail.com>
2025-05-10 23:04:05 -0300

    added Rating Factory

---
+++ database/factories/RatingFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Recipe;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Rating>
+ */
+class RatingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $recipeId = Recipe::inRandomOrder()->value('id');
+        return [
+            'recipe_id' => $recipeId,
+            'rating' => $this->faker->numberBetween(1,5),
+        ];
+    }
+}
