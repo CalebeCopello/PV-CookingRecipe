5a6781aed7127744299bf7b5cc7a35d71d872662
Calebe Copello <calebecopello@gmail.com>
2025-05-10 22:07:19 -0300

    fix typo

--- database/migrations/2025_05_11_003936_create_ratings_table.php
+++ database/migrations/2025_05_11_003936_create_ratings_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('ratings', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('recipe_id')->constrained()->onDelete('caascade');
+            $table->foreignId('recipe_id')->constrained()->onDelete('cascade');
             $table->tinyInteger('rating')->unsigned();
             $table->timestamps();
         });
